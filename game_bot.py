7afbb06d7d0ae976ec94cb01c1586b42b3ace4e4
Yurii Zablockii <zablockiiyua@yandex.ru>
2023-10-24 20:56:18 +0700

    рабочий бот угадай число на несколько пользователей, но не проверял на других. При этом криво стоят функции /help, /stat, /cancel потому что не хотел в них писать кода на регистрацию нового юзера.

--- game_bot.py
+++ game_bot.py
@@ -4,11 +4,7 @@ from aiogram import  Bot, Dispatcher, F
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
 
-user = {'in_game' : False,
-        'secret_number' : None,
-        'attempts' : None,
-        'total_games' : 0,
-        'wins' : 0}
+users = {}
 
 BOT_TOKEN = '6407812037:AAHIjdQDfRW4JwyldWtwTN9ApORUEKG4XHk'
 
@@ -23,8 +19,17 @@ def get_random_number() -> int:
 async def process_start_command(message: Message):
     await message.answer(
         'Привет!\nДавайте сыграем в игру "Угадай число"?\n\n'
+        'чтобы начать играть введите да или давай\n\n'
         'Чтобы получить правила игры и список доступных '
-        'команд - отправьте команду /help'
+        'команд - отправьте команду    /help_new_user'
+    )
+
+@dp.message(Command(commands='help_new_user'))
+async def process_help_command(message: Message):
+    await message.answer(
+        f'Правила игры:\n\nЯ загадываю число от 1 до 100, '
+        f'а вам нужно его угадать\nУ вас есть {ATTEMPTS} '
+        f'попыток, давайте сыграем?'
     )
 
 @dp.message(Command(commands='help'))
@@ -40,14 +45,14 @@ async def process_help_command(message: Message):
 @dp.message(Command(commands='stat'))
 async def process_stat_command(message: Message):
     await message.answer(
-        f'всего игр сыграно: {user["total_games"]}\n'
-        f'Игр выиграно: {user["wins"]}'
+        f'всего игр сыграно: {users[message.from_user.id]["total_games"]}\n'
+        f'Игр выиграно: {users[message.from_user.id]["wins"]}'
     )
 
 @dp.message(Command(commands='cancel'))
 async def process_cancel_command(message: Message):
-    if user['in_game']:
-        user['in_game'] = False
+    if users[message.from_user.id]['in_game']:
+        users[message.from_user.id]['in_game'] = False
         await message.answer(
             'Вы вышли из игры'
         )
@@ -58,11 +63,22 @@ async def process_cancel_command(message: Message):
         )
 @dp.message(F.text.lower().in_(['да','давай','игра']))
 async def process_positive_answer(message: Message):
-    if not user['in_game']:
-        user['in_game'] = True
-        user['secret_number'] = get_random_number()
-        user['attempts'] = ATTEMPTS
+    if message.from_user.id not in users:
+        users[message.from_user.id]: dict = {
+            'in_game': False,
+            'secret_number': None,
+            'attempts': None,
+            'total_games': 0,
+            'wins': 0
+        }
+    if not users[message.from_user.id]['in_game']:
+        users[message.from_user.id]['in_game'] = True
+        users[message.from_user.id]['secret_number'] = get_random_number()
+        users[message.from_user.id]['attempts'] = ATTEMPTS
         await message.answer(
+            f'Доступные команды:\n/help - правила '
+            f'игры и список команд\n/cancel - выйти из игры\n'
+            '/stat - посмотреть статистику)\n\n'
             'Ура!\n\nЯ загадал число от 1 до 100, '
             'попробуй угадать!'
         )
@@ -75,7 +91,7 @@ async def process_positive_answer(message: Message):
 
 @dp.message(F.text.lower().in_(['нет','не хочу','не','не буду']))
 async def process_negativ_answer(message: Message):
-    if not user['in_game']:
+    if not users[message.from_user.id]['in_game']:
         await message.answer(
             'Жаль :(\n\nЕсли захотите поиграть - просто '
             'напишите об этом'
@@ -85,29 +101,30 @@ async def process_negativ_answer(message: Message):
 
 @dp.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 100)
 async def process_numbers_answer(message: Message):
-    if user['in_game']:
-        if int(message.text) == user['secret_number']:
-            user['in_game'] = False
-            user['total_games'] += 1
-            user['wins'] += 1
+    if users[message.from_user.id]['in_game']:
+        if int(message.text) == users[message.from_user.id]['secret_number']:
+            users[message.from_user.id]['in_game'] = False
+            users[message.from_user.id]['total_games'] += 1
+            users[message.from_user.id]['wins'] += 1
             await message.answer(
                 'Ура!!! Вы угадали число!\n\n'
                 'Может, сыграем еще?'
             )
-        elif int(message.text) < user['secret_number']:
-            user['attempts'] -= 1
-            print({user["secret_number"]})
+            print(users)
+        elif int(message.text) < users[message.from_user.id]['secret_number']:
+            users[message.from_user.id]['attempts'] -= 1
+            print({users[message.from_user.id]["secret_number"]})
             await message.answer('мое число больше')
-        elif int(message.text) > user['secret_number']:
-            user['attempts'] -= 1
+        elif int(message.text) > users[message.from_user.id]['secret_number']:
+            users[message.from_user.id]['attempts'] -= 1
             await message.answer('мое число меньше')
-        elif user['attempts'] == 0:
-            user['in_game'] = False
-            user['total_games'] += 1
+        elif users[message.from_user.id]['attempts'] == 0:
+            users[message.from_user.id]['in_game'] = False
+            users[message.from_user.id]['total_games'] += 1
             await message.answer(
                 f'К сожалению, у вас больше не осталось '
                 f'попыток. Вы проиграли :(\n\nМое число '
-                f'было {user["secret_number"]}\n\nДавайте '
+                f'было {users[message.from_user.id]["secret_number"]}\n\nДавайте '
                 f'сыграем еще?'
             )
         else:
@@ -115,13 +132,21 @@ async def process_numbers_answer(message: Message):
 
 @dp.message()
 async def process_other_answer(message: Message):
-    if user['in_game']:
+    if message.from_user.id not in users:
+        users[message.from_user.id]: dict = {
+            'in_game': False,
+            'secret_number': None,
+            'attempts': None,
+            'total_games': 0,
+            'wins': 0
+        }
+    if users[message.from_user.id]['in_game']:
         await message.answer(
             'Мы же сейчас с вами играем. '
             'Присылайте, пожалуйста, числа от 1 до 100'
         )
     else:
-        await message.answer('не дури, играй или нахуй иди!')
+        await message.answer('нажмите /start')
 
 if __name__ == '__main__':
     dp.run_polling(bot)
